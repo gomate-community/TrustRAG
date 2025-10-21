ebd897d58f0cc9ee1bb3b80e79bb56330dad6049
yanqiangmiffy <1185918903@qq.com>
2025-10-21 16:21:39 +0800

    update@update engine example

--- examples/engine/xinference_example.py
+++ examples/engine/xinference_example.py
@@ -1,10 +1,12 @@
 import openai
 
 client = openai.Client(
-  api_key="cannot be empty",
-  base_url="http://<XINFERENCE_HOST>:<XINFERENCE_PORT>/v1"
+  api_key="api-key",
+  base_url="http://localhost:9997/v1"
 )
-client.embeddings.create(
+response=client.embeddings.create(
   model="bge-m3",
   input=["What is the capital of China?"]
 )
+print(type(response.data[0].embedding),len(response.data[0].embedding),response.data[0].embedding,)
+# <class 'list'> 1024 [-0.031030284240841866, ]
